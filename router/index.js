07d8a08c7f42b15b7cbcffa5ceabc9f8977a7f22
張睿峰 <turtle@zhangruifengdeMacBook-Air.local>
2016-08-11 15:47:48 +0800

    front-endFunctionComplete

--- router/index.js
+++ router/index.js
@@ -23,6 +23,7 @@ router.get('/', function(req, res) {
     res.sendFile(path.resolve('view/index.html'));
 });
 
+//用前端送資料寫法
 router.post('/send', function(req, res, next) {
     console.log(req.body);
     var ItemName = "";
@@ -71,30 +72,108 @@ router.post('/send', function(req, res, next) {
         url: 'https://payment-stage.allpay.com.tw/AioHelper/GenCheckMacValue',
         form: form1
     }, function(err, httpResponse, body) {
-        // console.log(body);
-        var form2 = {
-                MerchantID: 2000132,
-                MerchantTradeNo: TradeNo,
-                MerchantTradeDate: time,
-                PaymentType: 'aio',
-                TotalAmount: req.body.TotalAmount,
-                TradeDesc: 'allpay 商城購物-',
-                ItemName: ItemName,
-                ReturnURL: rURL,
-                ChoosePayment: req.body.ChoosePayment,
-                CheckMacValue: body
-            }
-            // console.log(form2);
-        request.post({
-            url: 'https://payment-stage.allpay.com.tw/Cashier/AioCheckOut/V2',
-            form: form2
-        }, function(err, httpResponse, body) {
-            $ = cheerio.load(body);
-            $("head").prepend('<base href="https://payment-stage.allpay.com.tw" target="_blank"/>');
-            res.send($.html());
-        });
+        //從歐付寶取得檢查碼後，組合成一個form
+        console.log(body);
+        var form = `
+        傳送中...
+        <form id="form" method="post" action="https://payment-stage.allpay.com.tw/Cashier/AioCheckOut/V2">
+            <input type="hidden" name="MerchantID" value="${form1.MerchantID}">
+            <input type="hidden" name="MerchantTradeNo" value="${form1.MerchantTradeNo}">
+            <input type="hidden" name="MerchantTradeDate" value="${form1.MerchantTradeDate}">
+            <input type="hidden" name="PaymentType" value="${form1.PaymentType}">
+            <input type="hidden" name="TotalAmount" value="${form1.TotalAmount}">
+            <input type="hidden" name="TradeDesc" value="${form1.TradeDesc}">
+            <input type="hidden" name="ItemName" value="${form1.ItemName}">
+            <input type="hidden" name="ReturnURL" value="${form1.ReturnURL}">
+            <input type="hidden" name="ChoosePayment" value="${form1.ChoosePayment}">
+            <input type="hidden" name="CheckMacValue" value="${body}">
+        </form>
+         <script>
+             document.getElementById('form').submit();
+         </script>
+        `;
+        //加入自動送出的script後，回傳給前端
+        res.send(form);
+        console.log(form);
     });
 });
+
+
+
+
+//用後端送資料寫法
+// router.post('/send', function(req, res, next) {
+//     console.log(req.body);
+//     var ItemName = "";
+//     var ItemName1 = req.body.bag1,
+//         ItemName2 = req.body.bag2,
+//         ItemName3 = req.body.bag3;
+//     if (ItemName1 == 0 && ItemName2 == 0) {
+//         ItemName += "小丑女零錢包" + ItemName3 + "個";
+//     } else if (ItemName1 == 0 && ItemName3 == 0) {
+//         ItemName += "柴犬零錢包" + ItemName2 + "個";
+//     } else if (ItemName2 == 0 && ItemName3 == 0) {
+//         ItemName += "柯基零錢包" + ItemName1 + "個";
+//     } else {
+//         ItemName += "柯基零錢包" + ItemName1 + "個," + "柴犬零錢包" + ItemName2 + "個," + "小丑女零錢包" + ItemName3 + "個";
+//     }
+
+//     var date = new Date();
+//     var Month = date.getMonth(),
+//         Day = date.getDay(),
+//         Hours = date.getHours(),
+//         Minutes = date.getMinutes(),
+//         Seconds = date.getSeconds();
+//     var time = date.getFullYear() + '/' + (Month < 10 ? '0' : '') + Month + '/' + (Day < 10 ? '0' : '') + Day + ' ' + (Hours < 10 ? '0' : '') + Hours + ':' + (Minutes < 10 ? '0' : '') + Minutes + ':' + (Seconds < 10 ? '0' : '') + Seconds;
+//     var time1 = date.getFullYear() + (Month < 10 ? '0' : '') + Month + (Day < 10 ? '0' : '') + Day + (Hours < 10 ? '0' : '') + Hours + (Minutes < 10 ? '0' : '') + Minutes + (Seconds < 10 ? '0' : '') + Seconds;
+//     var TradeNo = 'coin' + time1;
+//     console.log(ItemName);
+//     var form1 = {
+//             MerchantID: 2000132, //廠商編號(由allpay 提供)
+//             MerchantTradeNo: TradeNo, //廠商交易編號(由商家自訂)
+//             MerchantTradeDate: time, //廠商交易時間
+//             PaymentType: 'aio', //交易類型
+//             TotalAmount: req.body.TotalAmount, //交易金額
+//             TradeDesc: 'allpay 商城購物-', //交易描述
+//             ItemName:  ItemName, //商品名稱
+//             ReturnURL: rURL, //回傳網址
+//             ChoosePayment: req.body.ChoosePayment //付款方式
+//         }
+//         firebase.database().ref('/allpaydata').push({
+//             Name: req.body.Name,
+//             Email: req.body.Email,
+//             Phone: req.body.Phone,
+//             Place: req.body.Place,
+//             MerchantTradeNo: TradeNo
+//         });
+//     request.post({
+//         url: 'https://payment-stage.allpay.com.tw/AioHelper/GenCheckMacValue',
+//         form: form1
+//     }, function(err, httpResponse, body) {
+//         // console.log(body);
+//         var form2 = {
+//                 MerchantID: 2000132,
+//                 MerchantTradeNo: TradeNo,
+//                 MerchantTradeDate: time,
+//                 PaymentType: 'aio',
+//                 TotalAmount: req.body.TotalAmount,
+//                 TradeDesc: 'allpay 商城購物-',
+//                 ItemName: ItemName,
+//                 ReturnURL: rURL,
+//                 ChoosePayment: req.body.ChoosePayment,
+//                 CheckMacValue: body
+//             }
+//             // console.log(form2);
+//         request.post({
+//             url: 'https://payment-stage.allpay.com.tw/Cashier/AioCheckOut/V2',
+//             form: form2
+//         }, function(err, httpResponse, body) {
+//             $ = cheerio.load(body);
+//             $("head").prepend('<base href="https://payment-stage.allpay.com.tw" target="_blank"/>');
+//             res.send($.html());
+//         });
+//     });
+// });
 
 
 
